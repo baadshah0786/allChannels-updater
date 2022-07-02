314a4847493c9dc5e51bfcc17bbd8de6735cf3dc
Baadshah0786 <87961897+baadshah0786@users.noreply.github.com>
2022-07-02 18:06:08 +0530

    Update allChannels.py

--- code_samples/allChannels.py
+++ code_samples/allChannels.py
@@ -1,9 +1,9 @@
-### Script to get all channels from Tata Play IPTV
+### Script to get all channels from tata sky
 import threading
 import requests
 import json as json
 
-API_BASE_URL = "https://ts-api.videoready.tv/"
+API_BASE_URL = "https://kong-tatasky.videoready.tv/"
 
 channel_list = []
 
@@ -42,7 +42,7 @@ def processChnuks(channel_lists):
 
 def getAllChannels():
     ts = []
-    url = API_BASE_URL + "content-detail/pub/api/v1/channels?limit=999"
+    url = API_BASE_URL + "content-detail/pub/api/v1/channels?limit=1000"
     x = requests.get(url)
     channel_list = x.json()['data']['list']
     print("Total Channels fetched:", len(channel_list))
@@ -59,4 +59,3 @@ def getAllChannels():
 
 if __name__ == '__main__':
     getAllChannels()
-
